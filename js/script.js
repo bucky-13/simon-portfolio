e2e4a904d124753b330720958fc132dad0b3b8b6
bucky-13 <simon.stenstrom.013@gmail.com>
2023-01-03 15:53:25 +0100

    fixed menu

--- js/script.js
+++ js/script.js
@@ -3,17 +3,43 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const hamburgerX = document.querySelector(".hamburger-x");
 const menu = document.querySelector(".menu");
+const mobileMenuHidden = document.querySelector(".mobile-menu-hidden");
+const mobileMenu = document.querySelector(".mobile-menu");
 const contentContainer = document.querySelector(".content-container");
 
+//hamburger menu functions
+
+const hamburgerToggle = function () {
+  hamburger.classList.toggle("hidden");
+  hamburgerX.classList.toggle("hidden");
+  menu.classList.toggle("mobile-menu-hidden");
+  menu.classList.toggle("mobile-menu");
+};
+
+const closeHamburger = function () {
+  hamburgerToggle();
+  contentContainer.style.gridTemplateRows = "80px auto 50px";
+};
+
+//hamburger event listeners
+
+//open hamburger menu
+
 hamburger.addEventListener("click", function () {
-  hamburger.classList.add("hidden");
-  hamburgerX.classList.remove("hidden");
-  menu.style.display = "inline-block";
+  hamburgerToggle();
   contentContainer.style.gridTemplateRows = "400px auto 50px";
 });
 
+//close hamburger menu
+
 hamburgerX.addEventListener("click", function () {
-  hamburger.classList.remove("hidden");
-  hamburgerX.classList.add("hidden");
-  menu.style.display = "none";
+  closeHamburger();
+});
+
+window.addEventListener("resize", function () {
+  let h = document.documentElement.clientWidth;
+  if (h > 900 && !menu.classList.contains("mobile-menu-hidden")) {
+    closeHamburger();
+    console.log("closed");
+  }
 });
