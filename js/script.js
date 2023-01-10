b4cadd436db4fce98f9b1d1c4cf8465c7dc23964
bucky-13 <simon.stenstrom.013@gmail.com>
2023-01-10 17:27:20 +0100

    added validation for contact form

--- js/script.js
+++ js/script.js
@@ -43,3 +43,68 @@ window.addEventListener("resize", function () {
     console.log("closed");
   }
 });
+
+//form validation
+
+const form = document.querySelector("#send-message");
+const fullname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  const fullnameValue = fullname.value.trim();
+  const emailValue = email.value.trim();
+  const msgValue = message.value.trim();
+  let n = false;
+  let e = false;
+  let m = false;
+
+  if (fullnameValue === "") {
+    setErrorInput(fullname, "You need to enter your name.");
+  } else {
+    setSuccessInput(fullname);
+    n = true;
+  }
+
+  if (emailValue === "") {
+    setErrorInput(email, "You need to enter a valid e-mail address.");
+  } else {
+    setSuccessInput(email);
+    e = true;
+  }
+
+  if (msgValue === "") {
+    setErrorInput(message, "You need to enter a message.");
+  } else {
+    setSuccessInput(message);
+    m = true;
+  }
+
+  if (n === true && e === true && m === true) {
+    form.submit();
+  }
+}
+
+function setErrorInput(input, errorMessage) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = errorMessage;
+  formControl.className = "form-control error";
+}
+
+function setSuccessInput(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function validateEmail(email) {
+  let regular_expressions =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regular_expressions.test(String(email).toLocaleLowerCase());
+}
