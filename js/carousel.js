96aa13626fb84e9d546850fc65100438d164ea90
bucky-13 <simon.stenstrom.013@gmail.com>
2023-01-10 15:45:33 +0100

    added portfolio form start

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+// const elem = document.querySelector(".main-gallery");
+// let flkty = new Flickity(elem, {
+//   // options
+//   cellAlign: "left",
+//   contain: true,
+//   autoPlay: true,
+//   wrapAround: true,
+//   setGallerySize: false,
+// });
